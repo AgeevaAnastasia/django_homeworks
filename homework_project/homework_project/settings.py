d2e2e5b533ca158b76154781cf18e751e3936807
Anastasia Ageeva <anastasia.smirnova@gmail.com>
2023-09-08 12:52:32 +0300

    preparing for deploying

--- homework_project/homework_project/settings.py
+++ homework_project/homework_project/settings.py
@@ -20,13 +20,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5gj&%+#2$jawhl^p1qi!^e=%37#_zuqr%szwe938w94x4f4tm3'
+#SECRET_KEY = 'django-insecure-5gj&%+#2$jawhl^p1qi!^e=%37#_zuqr%szwe938w94x4f4tm3'
+
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
+ALLOWED_HOSTS = [
+    'zzu.pythonanywhere.com',
+]
+
+INTERNAL_IPS= [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -75,12 +87,27 @@ WSGI_APPLICATION = 'homework_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zzu$default',
+        'USER': 'zzu',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'zzu.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
+
 
 
 # Password validation
@@ -105,7 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
